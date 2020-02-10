39d6ff46801888bedc120bd3099813b7f68e60b4
Marco Müllner <muellnermarco@gmail.com>
2020-02-10 14:06:43 +0100

    init commit

---
+++ GUI/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow wnd = new MainWindow();
+            //TODO run startup stuff from here
+            //init Hardware
+            //init DB
+            wnd.Show();
+        }
+
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ GUI/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using CustomUserControl;
+
+namespace GUI
+{
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void show_info_page(object sender, RoutedEventArgs e)
+        {
+            InfoPage info_page = this.MainGrid.FindName("UCInformation") as InfoPage;
+
+            //show info page, hide the rest
+            info_page.Visibility = Visibility.Visible;
+        }
+
+        private void show_prod_page(object sender, RoutedEventArgs e)
+        {
+            InfoPage info_page = this.MainGrid.FindName("UCInformation") as InfoPage;
+
+            //show info page, hide the rest
+            info_page.Visibility = Visibility.Collapsed;
+        }
+
+        private void show_assistant_page(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void show_parameter_page(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
